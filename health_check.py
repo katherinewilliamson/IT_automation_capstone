102160e64f1a730c2ee279ee938bb407bb4bc23d
Katherine <mail@katherinewilliamson.work>
2024-01-23 16:13:59 -0600

    Added functions in health check to check disk, memory, and local host, and to send an email if one of the checks fails.

--- health_check.py
+++ health_check.py
@@ -1,16 +1,72 @@
 #!/usr/bin/env python3
 
-import psutil
+import subprocess
+import sys
+import socket
+import emails
+try:
+    import psutil
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'psutil'])
+    import psutil
 
-cpuPercentage = psutil.cpu_percent(interval=1)
+def checkCPU():
+    cpuPercentage = psutil.cpu_percent(interval=1)
+    if cpuPercentage > 80:
+        error = "Error - CPU usage is over 80%"
+        return error
+    else:
+        return None
 
-memory = psutil.virtual_memory()
 
-threshold = 500 * 1024 * 1024
-if memory.available <= threshold:
+def checkHost():
+    try:
+        resolvedAddress = socket.gethostbyname("localhost")
+        if resolvedAddress != "127.0.0.1":
+            raise socket.error
+        return None
+    except socket.error:
+        error = "Error - localhost cannot be resolved to 127.0.0.1"
+        return error
+
+def checkMemory():
+    memory = psutil.virtual_memory()
+    threshold = 500 * 1024 * 1024
+    if memory.available <= threshold:
+        error = "Error - Available memory is less than 500MB"
+        return error
+    else:
+        return None
     
-    
-    
-diskUsage = psutil.disk_usage('/')
-if diskUsage.percent >= 80:
-    
+def checkDisk():
+    diskUsage = psutil.disk_usage('/')
+    if diskUsage.percent >= 80:
+        error = "Error - Available disk space is less than 20%"
+        return error
+    else:
+        return None
+
+def reportError(message):
+    sender = "automation@example.com"
+    recipient =
+    body = "Please check your system and resolve the issue as soon as possible."
+    email = emails.generate_email(sender, recipient, message, body)
+    emails.send_email(email)
+
+if __name__ == "__main__":
+    errorMessages = []
+    cpuError = checkCPU()
+    if cpuError:
+        errorMessages.append(cpuError)
+    hostError = checkHost()
+    if hostError:
+        errorMessages.append(hostError)
+    memoryError = checkMemory()
+    if memoryError:
+        errorMessages.append(memoryError)
+    diskError = checkDisk()
+    if diskError:
+        errorMessages.append(diskError)
+    if errorMessages:
+        for error in errorMessages:
+            reportError(error)
