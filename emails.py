98df670d517e0efd81f0bad514497852476c9e34
Katherine <mail@katherinewilliamson.work>
2024-01-23 15:12:07 -0600

    Bug fixes and formatting changes.

--- emails.py
+++ emails.py
@@ -2,22 +2,25 @@
 from email.message import EmailMessage
 import os
 import mimetypes
+import smtplib
+
 
 def generate_email(sender, recipient, subject, body, attachment):
     email = EmailMessage()
     email["From"] = sender
     email["To"] = recipient
     email["Subject"] = subject
-    email.setcontent(body)
+    email.set_content(body)
     attachmentName = os.path.basename(attachment)
     mime_type,_ = mimetypes.guess_type(attachment)
     mime_type, mime_subtype = mime_type.split("/",1)
     with open(attachment, "rb") as attachmentFile:
-        message.add_attachment(attachmentFile.read(), maintype=mime_type, subtype=mime_subtype, filename=attachmentName)
+        email.add_attachment(attachmentFile.read(), maintype=mime_type, subtype=mime_subtype, filename=attachmentName)
     return email
     
+
 def send_email(email):
-  mailServer = smtplib.SMTP("localhost")
-  mailServer.send_message(email)
-  mailServer.quit()
+    mailServer = smtplib.SMTP("localhost")
+    mailServer.send_message(email)
+    mailServer.quit()
   
